eccdf1e5ce2a275d75aa83f26d5244f2d3395dca
RenatoStosl <renatosantoslourenco@gmail.com>
2024-05-11 14:04:42 -0300

    chore: Add Home component and update layout styling

---
+++ src/app/Home/Home.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Daniela Costa</h1>
+    </div>
+  );
+}
+
+export default Home;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
+import Home from "./Home/Home";
 
-export default function Home() {
+export default function HomePage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Daniela Costa</h1>
+      <Home />
     </main>
   );
 }
