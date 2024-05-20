c2f56d10ba7d31729b942f2699b614caa6739a7c
RenatoStosl <renatosantoslourenco@gmail.com>
2024-05-20 10:47:13 -0300

    chore: Update Home component layout and add responsive styling

--- src/app/Home/Home.tsx
+++ src/app/Home/Home.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 
 const Home = () => {
   return (
-    <div className="flex items-center w-8/12">
+    <div className="flex items-center w-8/12 flex-col lg:flex-row">
       <Title />
       <Image src="/daniela.png" alt="Daniela Costa" width={600} height={500} />
     </div>
